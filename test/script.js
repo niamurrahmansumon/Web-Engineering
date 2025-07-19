a2298808b314e97b95ae32caf8fcd1459c70cfe6
niamurrahmansumon <sniamurrahman@gmail.com>
2025-07-19 10:58:11 +0000

    Jul 19, 2025, 4:58 PM

--- test/script.js
+++
@@ -1,8 +0,0 @@
-function search() {
-  const input = document.querySelector('.search-box input');
-  if (input.value.trim() !== "") {
-    alert(`You searched for: ${input.value}`);
-  } else {
-    alert("Please enter a search term.");
-  }
-}
