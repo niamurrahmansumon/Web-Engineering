cebf6f7cdf54f4852aa61d42df7aad61a7252fb6
niamurrahmansumon <sniamurrahman@gmail.com>
2025-08-22 18:01:36 +0000

    Aug 23, 2025, 12:01 AM

--- Final/local.php
+++ Final/local.php
@@ -1,5 +1,8 @@
 <?php
 function myTest() {
     $x = 5;
-    echo "<p>The value of x is: $x";
+    echo "<p>Variable x inside function: $x</p>";
 }
+myTest();
+echo "<p>Variable x outside function: $x</p>";
+?>
